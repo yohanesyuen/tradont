e335a1ea6475b9b51d64bc6cc27e5cca4225dccd
Yohanes Yuen <hello@yohanesyuen.com>
2019-03-17 01:27:44 +0800

    Modify model variable name
    Modify tradont.model.CurrencyPair variable name
    Change symbol_name to symbol

--- tradont/model/CurrencyPair.py
+++ tradont/model/CurrencyPair.py
@@ -8,7 +8,7 @@ from tradont.model import Base
 class CurrencyPair(Base):
     __tablename__ = 'ccy_pairs'
     id = Column(Integer, Sequence('ccy_pair_id_seq'), primary_key=True)
-    symbol_name = Column(String(7), nullable=False)
+    name = Column(String(7), nullable=False)
     base_ccy_id = Column(Integer, ForeignKey('currencies.id'))
     base_ccy = relationship('Currency', foreign_keys=[base_ccy_id])
     quote_ccy_id = Column(Integer, ForeignKey('currencies.id'))
